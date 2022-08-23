ecffb3b715097e117cc0e1cb5d2da64f02aafdb9
CarlosMartinez <carlosmartinez.xq@gmail.com>
2022-08-22 23:41:40 -0400

    archivos eliminados

--- javasCript/funciones.js
+++
@@ -1,8 +0,0 @@
-const boton3 = document.getElementById("boton3");
-const resultado = document.getElementById("resultado");
-const boton1 = document.getElementById("boton1");
-boton1.addEventListener("click", function(){ 
-    const value = document.getElementById("mensaje").value;
-    resultado.innerHTML = value;
-    boton3.innerHTML
-})
